67164d7173ccf835f1e0b9bb8f0faf2f1b21d4e2
Veer2Charan <90391925+Veer2Charan@users.noreply.github.com>
2022-07-01 23:18:27 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,41 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAiuaI0Iz4cneW3N7VYRy9rey583xtsVnM",
+    authDomain: "kwitter-2afdb.firebaseapp.com",
+    databaseURL: "https://kwitter-2afdb-default-rtdb.firebaseio.com",
+    projectId: "kwitter-2afdb",
+    storageBucket: "kwitter-2afdb.appspot.com",
+    messagingSenderId: "66743022580",
+    appId: "1:66743022580:web:bc5f90dd807e4e7bf57fa1"
+  };
+  
+  
+  firebase.initializeApp(firebaseConfig);
+
+function addUser() {
+    user_name=document.getElementById("user_name").value;
+firebase.database().ref("/").child(user_name).update({
+  work:"studying"  
+})
+}
+
+function addRoom() {
+    room_name= document.getElementById("room-name").value;
+    localStorage.setItem("room_name")
+    window.location="kwitter_room.html"
+}
+
+function logout() {
+  localStorage.removeItem("user_name")
+  localStorage.removeItem("room_name")
+  window.location="kwitter.html"
+}
+
+function send() {
+  msg=document.getElementById("msg").value;
+  firebase.database().ref(room_name).puah({
+    name:user_name,
+    message:msg,
+    lokes:0
+  })
+  document.getElementById("msg").value="";
+}
